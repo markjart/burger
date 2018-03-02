80b95c4c2c2353ff88ce7800712fd8c7ea522525
markjart <markjart@gmail.com>
2018-03-02 12:52:36 -0600

    uploading JAWSDB_URL addition to github

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,19 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-  port: 3306,
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "burger_db"
-});
+if (process.env.JAWSDB_URL){
+	connection = mysql.createconnection(process.env.JAWSDB_URL);
+}
+else {
+	connection = mysql.createConnection({
+		port: 3306,
+		host: "localhost",
+		user: "root",
+		password: "",
+		database: "burger_db"
+	});
+};
 
 // Make connection.
 connection.connect(function(err) {
